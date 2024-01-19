fef844e1a06b8ea2e82c68d9ceb10bff967d5d68
Théo Piacentini <theo.piacentini@gmail.com>
2024-01-19 10:43:23 +0100

    definition des objets de l'énoncé

--- src/main/java/org/example/Projet.java
+++ src/main/java/org/example/Projet.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Projet {
+    String nom;
+    List<Tache> taches = new ArrayList<>() ;
+
+    public Projet(String nom) {
+        this.nom = nom;
+    }
+
+    int calculerDureeTotale(List<Tache> taches) {
+        return 0;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
---
+++ src/main/java/org/example/Tache.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Tache {
+
+    int duree;
+
+    public Tache(int duree) {
+        this.duree = duree;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+}
