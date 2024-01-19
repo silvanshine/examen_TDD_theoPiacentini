d52c7bb66587be8f5ca15244b95b8c54d6dd1c68
Théo Piacentini <theo.piacentini@gmail.com>
2024-01-19 14:14:06 +0100

    ajout de commentaires

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 public class Utils {
 
+    /** Calculer la durée totale d'une liste de tâches
+     * @param taches Liste de tâches
+     * @return La durée totale des tâches
+     */
     public static double calculerDureeTotale(List<Tache> taches) {
         double dureeTotale = 0;
 
         for (Tache tache : taches) {
+            // pas besoin de vérifier si la durée est négative car la classe Tache le fait déjà
             dureeTotale += tache.getDuree();
         }
 
