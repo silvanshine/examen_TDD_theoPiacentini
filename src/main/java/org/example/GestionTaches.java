5111c8cc3d3038d270a5903f2b11681735a0676c
Théo Piacentini <theo.piacentini@gmail.com>
2024-01-19 10:36:18 +0100

    init

---
+++ src/main/java/org/example/GestionTaches.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class GestionTaches {
+
+    public void ajouterTache(String titre, String description) {
+
+    }
+
+    public boolean completerTache(String titre) {
+
+        return false;
+    }
+
+    public boolean verifierTache(String titre) {
+
+        return false;
+    }
+}
